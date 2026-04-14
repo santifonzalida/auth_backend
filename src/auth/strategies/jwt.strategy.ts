768d7bb053f41612d46b5139894a4803f6670c1b
Santiago <santiifonzalida@gmail.com>
2026-04-14 00:17:15 -0300

    validacion de secreto jwt

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -6,12 +6,16 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(config: ConfigService) {
+    const secret = config.get<string>('JWT_SECRET');
+
+    if (!secret) {
+      throw new Error('JWT_SECRET no encontrada en las variables de entorno');
+    }
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get<string>('JWT_SECRET'),
     });
   }
-
   async validate(payload) {
     return payload;
   }
