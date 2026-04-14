a2cecda246b78361b95a79e92238d3caa424305a
Santiago <santiifonzalida@gmail.com>
2026-04-14 14:19:11 -0300

    habilitar CORS para app front

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    origin: [
+      'http://localhost:4200',
+      'https://loginfrontend-production-53c4.up.railway.app',
+    ], // Permitir solicitudes desde el frontend
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
